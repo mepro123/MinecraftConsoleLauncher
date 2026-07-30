8636b06f5f58545109b6ed094538c56d843012c3
mepro123 <thanaparitwongchanasit@gmail.com>
2026-07-30 19:07:35 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,12 +1,14 @@
-﻿Invoke-WebRequest https://github.com/mepro123/MinecraftConsoleLauncher/archive/refs/heads/main.zip -OutFile cl.zip
+[System.IO.File]::WriteAllText("install.ps1", @'
+Set-Location "$env:USERPROFILE"
+Invoke-WebRequest https://github.com/mepro123/MinecraftConsoleLauncher/archive/refs/heads/main.zip -OutFile cl.zip
 Expand-Archive cl.zip -DestinationPath . -Force
 Remove-Item cl.zip
 New-Item -ItemType Directory -Force -Path "$env:USERPROFILE\bin" | Out-Null
 @"
 @echo off
-cd /d "$PWD\MinecraftConsoleLauncher-main"
+cd /d "$env:USERPROFILE\MinecraftConsoleLauncher-main"
 call RunLauncher.bat
 "@ | Set-Content "$env:USERPROFILE\bin\ConsoleLauncher.bat"
-$env:PATH += ";$env:USERPROFILE\bin"
 [Environment]::SetEnvironmentVariable("PATH", "$([Environment]::GetEnvironmentVariable('PATH','User'));$env:USERPROFILE\bin", "User")
 Write-Host "Done! Close and reopen a terminal, then type ConsoleLauncher"
+'@)
