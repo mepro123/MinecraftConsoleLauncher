7ef03673454bc31805a8e1f0b40a04cf578c3d60
mepro123 <thanaparitwongchanasit@gmail.com>
2026-07-30 19:05:54 +0700

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,12 @@
+﻿Invoke-WebRequest https://github.com/mepro123/MinecraftConsoleLauncher/archive/refs/heads/main.zip -OutFile cl.zip
+Expand-Archive cl.zip -DestinationPath . -Force
+Remove-Item cl.zip
+New-Item -ItemType Directory -Force -Path "$env:USERPROFILE\bin" | Out-Null
+@"
+@echo off
+cd /d "$PWD\MinecraftConsoleLauncher-main"
+call RunLauncher.bat
+"@ | Set-Content "$env:USERPROFILE\bin\ConsoleLauncher.bat"
+$env:PATH += ";$env:USERPROFILE\bin"
+[Environment]::SetEnvironmentVariable("PATH", "$([Environment]::GetEnvironmentVariable('PATH','User'));$env:USERPROFILE\bin", "User")
+Write-Host "Done! Close and reopen a terminal, then type ConsoleLauncher"
